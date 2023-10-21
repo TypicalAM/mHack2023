e97c2447928c1f78ea01dd27170a72e19e574aa2
TypicalAM <tpam_piaseczny@protonmail.com>
2023-10-21 17:26:57 +0200

    Added a basic homepage

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,18 +1,21 @@
-import { AlertCircle } from 'lucide-react';
 import React from 'react';
-import { Alert, AlertDescription, AlertTitle } from '../components/ui/alert';
+import { Button } from '../components/ui/button';
 
 export default function Home() {
 	return (
-		<main>
-			<div className="text-2xl"> Gówno </div>
-			<Alert variant="destructive">
-				<AlertCircle className="h-4 w-4" />
-				<AlertTitle>Error</AlertTitle>
-				<AlertDescription>
-					Your session has expired. Please log in again.
-				</AlertDescription>
-			</Alert>
+		<main className="h-screen">
+			<div className="flex flex-col justify-center items-center w-screen h-2/5 bg-primary-60">
+				<p className="h-3/4"> icon1 </p>
+				<h1 className="h-1/2 text-2xl text-default font-bold text-secondary-20"> mTermin </h1>
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-screen h-fill bg-back-layer-1">
+				<h2 className="text-xl font-bold font-default h-1/4 mt-6"> Uwaga! </h2>
+				<div>
+					<p className="text-justify"> Dane o dostępności pochodzą z systemu NFZ-szpitale, przychodnie i inne placówki medyczne same udostępniają informacje o dostępności, w związku z czym mogą być one nieaktualne. Klikając "Dalej" akceptujesz Regulamin Usługi "mTermin" oraz Politykę Prywatności. </p>
+				</div>
+				<Button className="w-1/2 h-1/4 mt-6"> Dalej </Button>
+			</div>
 		</main>
 	)
 }
