6f399a23464515619d485dea15f32537f6df60d2
TypicalAM <tpam_piaseczny@protonmail.com>
2023-10-21 19:24:17 +0200

    Minor fixes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 import requests
 import json
 from dataclasses import dataclass
@@ -33,53 +33,57 @@ class Pages:
     first: str
     last: str
 
+
+@dataclass
+class Input:
+    benefit: str
+    province: str
+    locality: str
+
 objects=[]
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def index():
-    errors=[]   
-    benefit = request.args.get('benefit')
-    provincy = request.args.get('provincy')
-    locality = request.args.get('locality')
+    data = request.get_json()
+    if not data:
+        return make_response("No data provided", HTTPStatus.BAD_REQUEST)
 
-    
+    try:
+        input_data = Input(**data) 
+    except TypeError as e:
+        return make_response(f"Invalid data provided: {e}", HTTPStatus.BAD_REQUEST)
+
     objects.clear()
     try:
-        url=f"https://api.nfz.gov.pl/app-itl-api/queues?page=1&limit=10&format=json&case=1&benefit={benefit}&provincy={provincy}&locality={locality}"
+        url=f"https://api.nfz.gov.pl/app-itl-api/queues?page=1&limit=10&format=json&case=1&benefit={input_data.benefit}&provincy={input_data.province}&locality={input_data.locality}"
         r=requests.get(url)
-        
     except:
-        errors.append(
-            "Unable to get URL. Please make sure it's valid and try again."
-        )
-    if r:
-        json_obj=json.loads(r.text)
-        pages=Pages(json_obj["meta"]["count"], json_obj["links"]["next"], json_obj["links"]["prev"], 
-                    json_obj["links"]["self"], json_obj["links"]["first"], json_obj["links"]["last"])
-        #json_formatted=json.dumps(json_obj, indent=4)
-        json_cut=json_obj["data"]
-        results=json_cut
-        for i in results:
-            objects.append(Kolejka(i["attributes"]["provider"], i["attributes"]["place"], i["attributes"]["benefit"], i["attributes"]["address"], 
-                                    i["attributes"]["locality"], i["attributes"]["phone"], i["attributes"]["latitude"], i["attributes"]["longitude"], 
-                                    i["attributes"]["statistics"]["provider-data"]["awaiting"], i["attributes"]["statistics"]["provider-data"]["average-period"], 
-                                    i["attributes"]["dates"]["date-situation-as-at"], i["attributes"]["dates"]["date"]))
-        
-        print(len(results))
+        return make_response("Unable to get URL. Please make sure it's valid and try again.", HTTPStatus.BAD_REQUEST)
+
+    if not r:
+        return make_response("Unable to get URL. Please make sure it's valid and try again.", HTTPStatus.BAD_REQUEST)
+
+    json_obj=json.loads(r.text)
+    count = json_obj["meta"]["count"]
+    if not count:
+        return make_response("No results found. Please make sure it's valid and try again.", HTTPStatus.NOT_FOUND)
+
+    links=json_obj["links"]
+    pages = Pages(count, links["next"], links["prev"], links["self"], links["first"], links["last"])
+    #json_formatted=json.dumps(json_obj, indent=4)
+    data = json_obj["data"]
+    for i in data:
+       objects.append(Kolejka(i["attributes"]["provider"], i["attributes"]["place"], i["attributes"]["benefit"], i["attributes"]["address"], i["attributes"]["locality"], i["attributes"]["phone"], i["attributes"]["latitude"], i["attributes"]["longitude"], i["attributes"]["statistics"]["provider-data"]["awaiting"], i["attributes"]["statistics"]["provider-data"]["average-period"],i["attributes"]["dates"]["date-situation-as-at"], i["attributes"]["dates"]["date"]))
 
     #return render_template('index.html', errors=errors, objects=objects, pages=pages)
     return send_json(pages, objects)
 
+
 @app.route('/api/json', methods=['GET'])
 def send_json(pages, objects):
-    print(jsonify(pages, objects))
-    return jsonify(pages, objects)
-
-
-def i_pretty_ask():
-    data = request.get_json()
-    print(data)
-    return make_response(200, "gitgud")
+    response = make_response(jsonify(pages, objects), HTTPStatus.OK)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
