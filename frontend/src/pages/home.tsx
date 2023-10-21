7558948d25287070d955aa2a2c1cc480776618e8
TypicalAM <tpam_piaseczny@protonmail.com>
2023-10-21 16:47:45 +0200

    Added shadcn ui library

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,18 @@
+import { AlertCircle } from 'lucide-react';
 import React from 'react';
+import { Alert, AlertDescription, AlertTitle } from '../components/ui/alert';
 
 export default function Home() {
 	return (
-		<div>This is the index page for the application!</div>
+		<main>
+			<div className="text-2xl"> Gówno </div>
+			<Alert variant="destructive">
+				<AlertCircle className="h-4 w-4" />
+				<AlertTitle>Error</AlertTitle>
+				<AlertDescription>
+					Your session has expired. Please log in again.
+				</AlertDescription>
+			</Alert>
+		</main>
 	)
 }
